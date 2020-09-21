bca30a2f20a5b5ae0bf4c0c821452bf14101f222
Elena Tsypileva <etsypileva@gmail.com>
2020-09-21 14:12:06 +0300

    added Lombok

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,78 +1,19 @@
 package ru.netology;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class Radio {
 
     private int stationsTotal = 10;
     private int stationNumber;
     private int volume;
 
-    public int getStationsTotal() {
 
-        return stationsTotal;
-    }
-
-    public void setStationsTotal(int stationsTotal) {
-        this.stationsTotal = stationsTotal;
-    }
-
-    public int getStationNumber() {
-
-        return stationNumber;
-    }
-
-    public void setStationNumber(int stationNumber) {
-        if (stationNumber > stationsTotal) {
-            return;
-        }
-        if (stationNumber < 0) {
-            return;
-        }
-
-        this.stationNumber = stationNumber;
-    }
-
-    public void nextStationNumber() {
-        if (stationNumber < stationsTotal) {
-            stationNumber = stationNumber + 1;
-        } else {
-            stationNumber = 0;
-        }
-    }
-
-    public void previousStationNumber() {
-        if (stationNumber > 0) {
-            stationNumber = stationNumber - 1;
-        } else {
-            stationNumber = stationsTotal;
-        }
-    }
-
-    public int getVolume() {
-
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        if (volume > 100) {
-            return;
-        }
-        if (volume < 0) {
-            return;
-        }
-
-        this.volume = volume;
-    }
-
-    public void increaseVolume() {
-
-        if (volume < 100) {
-            volume = volume + 1;
-        }
-    }
-
-    public void decreaseVolume() {
-        if (volume > 0) {
-            volume = volume - 1;
-        }
-    }
 }
